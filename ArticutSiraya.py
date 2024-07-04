93980e6851bee5394b619f20ba740305971e2e02
Peter <peter.w@droidtown.co>
2024-07-05 01:43:59 +0800

    Init commit.

---
+++ ArticutSiraya.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import re
+
+class XP:
+    """
+    一個最基本的 XP 會有的參數。
+    """
+    def __init__(self):
+        self.headFirst = True
+
+class OBL(XP):
+    """
+    OBL-case 繼承 XP 的結構，設為一 headFirst 參數的節點。
+    """
+    def __init__(self):
+        self.TagSTR = "OBL"
+        return None
+
+class ArticutSiraya:
+    def __init__(self):
+        self.inputSTR = None
+        self.OBL = OBL()
+        self.OBLpat = re.compile(r"\bki\b", re.IGNORECASE)
+        return None
+
+    def oblMarker(self):
+        return re.sub(self.OBLpat, "<{TAG}>ki</{TAG}}>".format({"TAG":self.OBL.TagSTR}), self.inputSTR)
+
+    def parse(self, inputSTR, level="lv1"):
+        self.inputSTR = inputSTR
+        self.inputSTR = self.oblMarker()
+
+        return self.inputSTR
+
+
+if __name__ == "__main__":
+    inputSTR = "Sulat ki kavuilan ti Jesus Christus"
+    siraya = ArticutSiraya()
+    resultDICT = siraya.parse(inputSTR)
+    print(resultDICT)
