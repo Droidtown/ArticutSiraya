79f6a074b8684f5ad528b3b82b736e56943c2a35
PeterWolf <peterwolf.wang@gmail.com>
2024-09-07 15:35:47 +0800

    adjust code to an 'irresponsible status' and passit to Jenny.

--- DictionaryUI.py
+++ DictionaryUI.py
@@ -22,12 +22,50 @@ from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll, HorizontalScroll, Container
 from textual.widgets import Input, Markdown, Footer, Label
 
-def QueryPacker(queryLIST, dictionary, dictSTR="amis"):
+
+def load_amis_dictionary(file_path: str) -> dict:
+    data = json.load(open(file_path, encoding="utf-8"))
+    result = {f"{item['title']}": {
+        "definitions": [
+            {
+                "synonyms": definition.get("synonyms", []),
+                "def": definition.get("def", "").replace("￻", " ").replace("￹", " ").replace("￺", " ")
+            }
+            for definition in item["heteronyms"][0].get("definitions", [])
+        ]
+    }
+    for item in data}
+    return result
+
+def load_siraya_dictionary(file_path: str) -> dict:
+    data = json.load(open(file_path, encoding="utf-8"))
+    result = {f"{item['title']}": {
+        "definitions": item["heteronyms"][0].get("definitions", [])}
+    for item in data}
+    return result
+
+amisDICT_01 = load_amis_dictionary("dictionary/dict-amis.json")
+amisDICT_02 = load_amis_dictionary("dictionary/dict-amis-safolu.json")
+
+sirayaDICT_01 = load_siraya_dictionary("dictionary/dict.jenny.json")
+sirayaDICT_02 = load_siraya_dictionary("dictionary/dict.jenny2.json")
+
+def MatchingQuery(queryLIST, dictSTR="amis"):
     resultLIST = []
     if dictSTR == "amis":
         for q in queryLIST:
-            if q in dictionary:
-                for content in dictionary[q]["definitions"]:
+            if q in amisDICT_01:
+                for content in amisDICT_01[q]["definitions"]:
+                    resultLIST.append("## Definition:")
+                    resultLIST.append(content["def"])
+                    resultLIST.append("============")
+                    try:
+                        resultLIST.append("## Synonyms:")
+                        resultLIST.append("\n".join(content["synonyms"]))
+                    except:
+                        pass
+            if q in amisDICT_02:
+                for content in amisDICT_02[q]["definitions"]:
                     resultLIST.append("## Definition:")
                     resultLIST.append(content["def"])
                     resultLIST.append("============")
@@ -38,15 +76,22 @@ def QueryPacker(queryLIST, dictionary, dictSTR="amis"):
                         pass
     elif dictSTR == "siraya":
         for q in queryLIST:
-            if q in dictionary:
+            if q in sirayaDICT_01:
                 resultLIST.append("## Definition:")
                 resultLIST.append(q["definitions"][0]["def"])
                 resultLIST.append("## Synonyms:")
                 resultLIST.append("\n".join(q["definitions"][0]["synonyms"]))
                 resultLIST.append("============")
+            if q in sirayaDICT_02:
+                resultLIST.append("## Definition:")
+                resultLIST.append(q["definitions"][0]["def"])
+                resultLIST.append("## Synonyms:")
+                resultLIST.append("\n".join(q["definitions"][0]["synonyms"]))
+                resultLIST.append("============")
+
     return resultLIST
 
-def FuzzyQueryMaker(word, dictionary, dictSTR="amis"):
+def FuzzyQuery(word, dictSTR="amis"):
     resultLIST = []
     patternDICT = {"C":"[bcdfghjklmnpqrstvxz]",
                    "V":"[aeiuowyj]",
@@ -64,35 +109,55 @@ def FuzzyQueryMaker(word, dictionary, dictSTR="amis"):
             queryLIST.append(char)               #[bpdtkg]ob
     querySTR = "".join(queryLIST)
     queryPat = re.compile(querySTR)
-    keySTR = "\n".join([k for k in dictionary])
-    matches = set([q.group(0) for q in queryPat.finditer(keySTR)]) #queryPat.findall(k) != []:
-    matches = sorted(matches)
-    if matches:
-        for k in matches:
-            if dictSTR == "amis":
-                try:
-                    for content in dictionary[k]["definitions"]:
-                        resultLIST.append(f"## Match Entry: {k}")
-                        resultLIST.append("## Definition:")
-                        resultLIST.append(content["def"])
-                        resultLIST.append("============")
-                        try:
-                            resultLIST.append("## Synonyms:")
-                            resultLIST.append("\n".join(content["synonyms"]))
-                        except:
-                            pass
-                except:
-                    pass
-            elif dictSTR == "siraya":
-                try:
-                    resultLIST.append(f"## Match Entry: {k}")
-                    resultLIST.append("## Definition:")
-                    resultLIST.append(dictionary[k]["definitions"][0]["def"])
-                    resultLIST.append("## Synonyms:")
-                    resultLIST.append("\n".join(dictionary[k]["definitions"][0]["synonyms"]))
-                    resultLIST.append("============")
-                except:
-                    pass
+    if dictSTR == "amis":
+        keySTR_amis01 = "\n".join([k for k in amisDICT_01])
+        matches_amis01 = set([q.group(0) for q in queryPat.finditer(keySTR_amis01)]) #queryPat.findall(k) != []:
+        matches_amis01 = sorted(matches_amis01)
+        keySTR_amis02 = "\n".join([k for k in amisDICT_02])
+        matches_amis02 = set([q.group(0) for q in queryPat.finditer(keySTR_amis02)]) #queryPat.findall(k) != []:
+        matches_amis02 = sorted(matches_amis02)
+        if matches_amis01:
+            for k in matches_amis01:
+                if dictSTR == "amis":
+                    try:
+                        for content in amisDICT_01[k]["definitions"]:
+                            resultLIST.append(f"## Match Entry: {k}")
+                            resultLIST.append("## Definition:")
+                            resultLIST.append(content["def"])
+                            resultLIST.append("============")
+                            try:
+                                resultLIST.append("## Synonyms:")
+                                resultLIST.append("\n".join(content["synonyms"]))
+                            except:
+                                pass
+                    except:
+                        pass
+        if matches_amis02:
+            for k in matches_amis02:
+                if dictSTR == "amis":
+                    try:
+                        for content in amisDICT_02[k]["definitions"]:
+                            resultLIST.append(f"## Match Entry: {k}")
+                            resultLIST.append("## Definition:")
+                            resultLIST.append(content["def"])
+                            resultLIST.append("============")
+                            try:
+                                resultLIST.append("## Synonyms:")
+                                resultLIST.append("\n".join(content["synonyms"]))
+                            except:
+                                pass
+                    except:
+                        pass
+    elif dictSTR == "siraya":
+        try:
+            resultLIST.append(f"## Match Entry: {k}")
+            resultLIST.append("## Definition:")
+            resultLIST.append(dictionary[k]["definitions"][0]["def"])
+            resultLIST.append("## Synonyms:")
+            resultLIST.append("\n".join(dictionary[k]["definitions"][0]["synonyms"]))
+            resultLIST.append("============")
+        except:
+            pass
     return resultLIST
 
 def regex_search(word, dictionary, resultLIST, is_amis=True):
@@ -143,7 +208,7 @@ def regex_search(word, dictionary, resultLIST, is_amis=True):
                                         resultLIST.append("## Synonyms:")
                                         resultLIST.append("\n".join(i["synonyms"]))
                                     except:
-                                        pass                              
+                                        pass
             else:
                 try:
                     resultLIST.append("## Definition:")
@@ -167,26 +232,7 @@ def regex_search(word, dictionary, resultLIST, is_amis=True):
     #except Exception as e:
         #resultLIST.append(f"UNKNOWN ERROR: {str(e)}")
 
-def load_amis_dictionary(file_path: str) -> dict:
-    data = json.load(open(file_path, encoding="utf-8"))
-    result = {f"{item['title']}": {
-        "definitions": [
-            {
-                "synonyms": definition.get("synonyms", []),
-                "def": definition.get("def", "").replace("￻", " ").replace("￹", " ").replace("￺", " ")
-            }
-            for definition in item["heteronyms"][0].get("definitions", [])
-        ]
-    }
-    for item in data}
-    return result
 
-def load_siraya_dictionary(file_path: str) -> dict:
-    data = json.load(open(file_path, encoding="utf-8"))
-    result = {f"{item['title']}": {
-        "definitions": item["heteronyms"][0].get("definitions", [])}
-    for item in data}
-    return result
 
 class DictionaryApp(App):
     """Searches a dictionary API as-you-type."""
@@ -224,74 +270,50 @@ class DictionaryApp(App):
             await self.query_one("#siraya-results", Markdown).update("")
 
 
-# #Do our look-up here!   .*kan.*
+    # #Do our look-up here!   .*kan.*
     @work(exclusive=True)
     async def lookup_dictionary(self, word: str) -> None:  #if "[" "]" "_" "CVN" => string ; => regex
         resultLIST = []
-        resultLIST.append("# 🅰️ mis Dictionary")
-        def process_dictionary(word, dictionary, resultLIST):
-            if word in dictionary:
-                for i in dictionary[word]["definitions"]:
-                    resultLIST.append("## Definition:")
-                    resultLIST.append(i["def"])
-                    try:
-                        resultLIST.append("## Synonyms:")
-                        resultLIST.append("\n".join(i["synonyms"]))
-                    except:
-                        pass
-            #<regex search here>
-            else:
-                regex_search(word, dictionary, resultLIST, is_amis=True)
-            #</regex search here>
-        #logger.debug(amisDICT_01)
-        process_dictionary(word, amisDICT_01, resultLIST)
-        process_dictionary(word, amisDICT_02, resultLIST)
+
+
         if word == self.query_one(Input).value:
-            #markdown = self.make_word_markdown("\n".join(resultLIST))
-            self.query_one("#amis-results", Markdown).update("\n".join(resultLIST))
+            resultLIST.append("# 🅰️ mis Dictionary")
             if set("CVNLB").intersection(word):
-                resultLIST = FuzzyQueryMaker(word)
-            elif
-            
+                resultLIST = FuzzyQuery(word)
+            #elif "?" in word:
+                #resultLIST = PartialQuery(word)
             else:
-                resultLIST = MatchingQuery(word)
-                
-        resultLIST = []
-        resultLIST.append("# 🇸 iraya Dictionary #")
-        def process_siraya_dictionary(word, dictionary, resultLIST):
-            if word in dictionary:
-                resultLIST.append("## Definition:")
-                resultLIST.append(dictionary[word]["definitions"][0]["def"])
-                resultLIST.append("## Synonyms:")
-                resultLIST.append("\n".join(dictionary[word]["definitions"][0]["synonyms"]))
-            #<regex search here>
+                resultLIST = MatchingQuery([word], dictSTR="amis")
+
+            resultLIST.append("# 🇸 iraya Dictionary #")
+            if set("CVNLB").intersection(word):
+                resultLIST = FuzzyQuery(word)
+            #elif "?" in word:
+                #resultLIST = PartialQuery(word)
             else:
-                regex_search(word, dictionary, resultLIST, is_amis=False)
-            #</regex search here>
-        process_siraya_dictionary(word, sirayaDICT_01, resultLIST)
-        process_siraya_dictionary(word, sirayaDICT_02, resultLIST)
-
-        if word == self.query_one(Input).value:
-            #markdown = self.make_word_markdown("\n".join(resultLIST))
-            self.query_one("#siraya-results", Markdown).update("\n".join(resultLIST))
+                resultLIST = MatchingQuery([word], dictSTR="siraya")
 
 
-    @work(exclusive=True)
-    async def lookup_word(self, word: str) -> None:
-        """Looks up a word."""
-        url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+        self.query_one("#amis-results", Markdown).update("\n".join(resultLIST))
+        self.query_one("#siraya-results", Markdown).update("\n".join(resultLIST))
 
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
-            try:
-                results = response.json()
-            except Exception:
-                self.query_one("#results", Markdown).update(response.text)
-                return
 
-        if word == self.query_one(Input).value:
-            markdown = self.make_word_markdown(results)
-            self.query_one("#results", Markdown).update(markdown)
+    #@work(exclusive=True)
+    #async def lookup_word(self, word: str) -> None:
+        #"""Looks up a word."""
+        #url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+
+        #async with httpx.AsyncClient() as client:
+            #response = await client.get(url)
+            #try:
+                #results = response.json()
+            #except Exception:
+                #self.query_one("#results", Markdown).update(response.text)
+                #return
+
+        #if word == self.query_one(Input).value:
+            #markdown = self.make_word_markdown(results)
+            #self.query_one("#results", Markdown).update(markdown)
 
     def make_word_markdown(self, results: object) -> str:
         """Convert the results in to markdown."""
@@ -315,15 +337,11 @@ class DictionaryApp(App):
 
 if __name__ == "__main__":
 
-    amisDICT_01 = load_amis_dictionary("dictionary/dict-amis.json")
-    amisDICT_02 = load_amis_dictionary("dictionary/dict-amis-safolu.json")
-    #amisDICT_03 = load_dictionary("dictionary/dict-concised.audio.json")
-    sirayaDICT_01 = load_siraya_dictionary("dictionary/dict.jenny.json")
-    sirayaDICT_02 = load_siraya_dictionary("dictionary/dict.jenny2.json")
+
 
     #app = DictionaryApp()
     #app.run()
-    
+
     word = "NaCa"
-    f = FuzzyQueryMaker(word, amisDICT_01)
+    f = FuzzyQuery(word, amisDICT_01)
     pprint(f)
